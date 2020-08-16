99625fa6a8bdf62b78c709315b95e76f58847bd2
SohailKhanPAK <39166458+SohailKhanPAK@users.noreply.github.com>
2020-08-16 16:43:08 +0500

    js

--- app.js
+++ app.js
@@ -8,3 +8,43 @@ month = months[d.getMonth()];
 dau_digit= d.getDate();
 
 date.innerHTML=`${day} , ${month} ${dau_digit}`
+
+
+var content = document.getElementById('content')
+var textField = document.getElementById('textField')
+var n =0;
+function toDo(){
+   if (textField.value!=""){
+content.innerHTML+=`
+
+<div class="row content1 ${colors[n]}">
+
+    <div class="col-2 col-sm-1 mt-3 circle-icon">
+        <i class="fa fa-circle-thin fa-big" aria-hidden="true"></i>
+    </div>
+
+    <div class="col-8 col-sm-10 mt-3   text">
+        <p id="text">${textField.value}</p>
+    </div>
+
+    <div class="col-2 col-sm-1 mt-3   trash-icon">
+        <i class="fa fa-trash-o fa-big"  aria-hidden="true"></i>
+    </div>
+</div>
+
+`
+
+textField.value=""
+n++
+if (n>5) {
+  n=0
+}
+
+   }
+}
+
+
+refresh = () =>{
+
+    content.innerHTML=""
+}
